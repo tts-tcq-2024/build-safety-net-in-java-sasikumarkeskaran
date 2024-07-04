5702996baf6071a54ad920904a67f8f11a2f9d24
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-07-04 13:47:50 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -7,12 +7,33 @@ import org.junit.Test;
 public class SoundexTest {
 
     @Test
-    public void testEmptyString() {
-       //  assertTrue(Soundex.generateSoundex("")=="");
+    void testBasicNames() {
+        assertEquals("A123", Soundex.generateSoundex("Ashcraft"));
+        assertEquals("B652", Soundex.generateSoundex("Bartosz"));
     }
 
     @Test
-    public void testSingleCharacter() {
-         //assertTrue(Soundex.generateSoundex("A")=="A000");
+    void testEdgeCases() {
+        assertEquals("", Soundex.generateSoundex(null));
+        assertEquals("", Soundex.generateSoundex(""));
+        assertEquals("A000", Soundex.generateSoundex("A"));
+    }
+
+    @Test
+    void testComplexNames() {
+        assertEquals("H452", Soundex.generateSoundex("Hemingway"));
+        assertEquals("L000", Soundex.generateSoundex("Lee"));
+    }
+
+    @Test
+    void testNamesWithSimilarSoundingConsonants() {
+        assertEquals("T522", Soundex.generateSoundex("Thompson"));
+        assertEquals("P532", Soundex.generateSoundex("Peterson"));
+    }
+
+    @Test
+    void testNamesWithHWSeparatingSameSoundingConsonants() {
+        assertEquals("W252", Soundex.generateSoundex("Wright"));
+        assertEquals("P530", Soundex.generateSoundex("Pfister"));
     }
 }
