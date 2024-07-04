d834d3ae8d1ad74ca16bcea9451616d055c21146
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-07-04 12:21:17 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -9,21 +9,40 @@ public class Soundex {
 
         StringBuilder soundex = new StringBuilder();
         soundex.append(Character.toUpperCase(name.charAt(0)));
+
+        String encodedName = encodeName(name);
+        appendEncodedCharacters(soundex, encodedName);
+
+        padWithZeros(soundex);
+
+        return soundex.toString();
+    }
+
+    private static String encodeName(String name) {
+        StringBuilder encoded = new StringBuilder();
         char prevCode = getSoundexCode(name.charAt(0));
 
-        for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
+        for (int i = 1; i < name.length(); i++) {
             char code = getSoundexCode(name.charAt(i));
             if (code != '0' && code != prevCode) {
-                soundex.append(code);
+                encoded.append(code);
                 prevCode = code;
             }
         }
 
+        return encoded.toString();
+    }
+
+    private static void appendEncodedCharacters(StringBuilder soundex, String encodedName) {
+        for (int i = 0; i < encodedName.length() && soundex.length() < 4; i++) {
+            soundex.append(encodedName.charAt(i));
+        }
+    }
+
+    private static void padWithZeros(StringBuilder soundex) {
         while (soundex.length() < 4) {
             soundex.append('0');
         }
-
-        return soundex.toString();
     }
 
     private static char getSoundexCode(char c) {
